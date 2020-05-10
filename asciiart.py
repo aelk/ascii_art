e90c66fc5683d1bfbf327f7b9ec930302be10e8f
Andrew Elkouri <andrewelkouri@gmail.com>
2020-05-10 11:09:39 -0400

    constructed brightness matrix

--- asciiart.py
+++ asciiart.py
@@ -1,11 +1,26 @@
 from PIL import Image
 
-imageName = "Fernando_noronha.jpg"
-im = Image.open(imageName)
-col, row = im.size
+def convertRGBtoBrightness(pixels, col, row):
+    brightnessMatrix = [0] * (col * row)
+    idx = 0
+    for i in range(col):
+        for j in range(row):
+            r, g, b = pixels[i, j]
+            brightnessMatrix[idx] = (r + g + b) // 3
+            idx += 1
+    return brightnessMatrix
 
-print("Successfully constructed pixel matrix!")
-print("Pixel matrix size:", col, "x", row)
+def main():
+    imageName = "Fernando_noronha.jpg"
+    im = Image.open(imageName)
+    col, row = im.size
 
-pixels = im.load()
+    print("Successfully constructed pixel matrix!")
+    print("Pixel matrix size:", col, "x", row)
+    pixels = im.load()
+    brightnessMatrix = convertRGBtoBrightness(pixels, col, row)
+    print(brightnessMatrix)
+    print(len(brightnessMatrix))
 
+if __name__ == '__main__':
+    main()
