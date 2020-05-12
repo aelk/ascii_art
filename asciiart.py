c504a71a532233cc67ce0de15f56679909282875
Andrew Elkouri <andrewelkouri@gmail.com>
2020-05-11 20:09:05 -0400

    command-line file name + cleanup

--- asciiart.py
+++ asciiart.py
@@ -1,20 +1,20 @@
+import sys
 from PIL import Image
+
+MAX_PIXEL_VALUE = 255;
+ASCII_CHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
 
 def printASCIIart(asciiMatrix, col, row):
     for row in asciiMatrix:
-        line = [p+p for p in row]
+        line = [p+p+p for p in row]
         print("".join(line))
-
-def brightnessToASCII(brightnessValue):
-    asciiThinToBold = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-    asciiIndex = int((brightnessValue / 255) * (len(asciiThinToBold) - 1))
-    return asciiThinToBold[asciiIndex]
 
 def mapBrightnessToASCIIMatrix(brightness, col, row):
     asciiMatrix = [["" for x in range(col)] for y in range(row)] 
     for i in range(row):
         for j in range(col):
-            asciiMatrix[i][j] = brightnessToASCII(brightness[i][j])
+            asciiIndex = int((brightness[i][j] / MAX_PIXEL_VALUE) * (len(ASCII_CHARS) - 1))
+            asciiMatrix[i][j] = ASCII_CHARS[asciiIndex]
     return asciiMatrix
 
 def convertRGBtoBrightness(pixels, col, row):
@@ -25,20 +25,17 @@ def convertRGBtoBrightness(pixels, col, row):
             brightnessMatrix[i][j] = (r + g + b) / 3.0
     return brightnessMatrix
 
-def main():
-    imgName = "halo.jpg"
-    img = Image.open(imgName)
+def main(fileName):
+    img = Image.open(fileName)
 
-    img.thumbnail((640, 480))
+    img.thumbnail((1000, 200))
     pixels = list(img.getdata())
     pixels = [pixels[i:i+img.width] for i in range(0, len(pixels), img.width)]
 
     col, row = len(pixels[0]), len(pixels)
-    print("Pixel matrix size:", col, "x", row)
-
     brightnessMatrix = convertRGBtoBrightness(pixels, col, row)
     asciiMatrix = mapBrightnessToASCIIMatrix(brightnessMatrix, col, row)
     printASCIIart(asciiMatrix, col, row)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
